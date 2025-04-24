a103b2527e66b51f4fc79037643376dbf0b7359b
Zunair Ali <ranazunairali007@gmail.com>
2025-04-24 19:19:25 +0500

    signin, signup functionality

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,11 +1,12 @@
 import User from "../models/userModel.js";
 
-import bcryptjs from "bcryptjs";
+import jwt from 'jsonwebtoken';
+import bcrypt from "bcrypt";
 
-export const signup = async (req, res,next) => {
+export const signup = async (req, res, next) => {
   const { username, email, password } = req.body;
 
-  const hashedPassword = await bcryptjs.hash(password, 10);
+  const hashedPassword = await bcrypt.hash(password, 10);
 
   const newUser = new User({ username, email, password: hashedPassword });
 
@@ -16,4 +17,57 @@ export const signup = async (req, res,next) => {
   } catch (error) {
     next(error);
   }
+};
+
+
+export const signin = async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    const user = await User.findOne({ email });
+
+    if (!user) {
+      return res.status(400).json({ message: "Invalid credentials" });
+    }
+
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+
+    if (!isPasswordValid) {
+      return res.status(400).json({ message: "Invalid credentials" });
+    }
+
+    const token = jwt.sign(
+      {
+        id: user._id,
+        isAdmin: user.isAdmin || false, // in case you store this flag
+      },
+      process.env.JWT_SECRET,
+      { expiresIn: "7d" } // 7 days
+    );
+
+   
+    res
+      .cookie("token", token, {
+        httpOnly: true,
+        secure: true,
+        sameSite: "strict",
+      })
+      .status(200)
+      .json({message:"login successfull"});
+
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Failed to login!" });
+  }
+};
+
+export const logout = (req, res) => {
+  res
+    .clearCookie("token", {
+      httpOnly: true,
+      secure: true,
+      sameSite: "strict",
+    })
+    .status(200)
+    .json({ message: "Logout Successful" });
 };
