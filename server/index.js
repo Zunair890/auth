997872389723ddb379623ddf210463cdf789651e
Zunair Ali <ranazunairali007@gmail.com>
2025-04-24 17:18:31 +0500

    added routes,middleware

--- server/index.js
+++ server/index.js
@@ -1,11 +1,34 @@
 import express from "express";
 import { connectDB } from "./config/mongodb.js";
 import dotenv from "dotenv";
+import userRoutes from "./routes/userRoute.js";
+import authRoutes from "./routes/authRoute.js";
 dotenv.config();
 
-
 const app = express();
+
+// api endpoints
+app.use(express.json());
+
+app.use("/api/user", userRoutes);
+app.use("/api/auth", authRoutes);
+
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+
+  const message = err.message || "Internal Server Error";
+
+  return res.status(statusCode).json({
+    success: false,
+
+    message,
+
+    statusCode,
+  });
+});
+
 await connectDB();
+
 app.listen(3000, () => {
   console.log("Server listening on port 3000");
 });
